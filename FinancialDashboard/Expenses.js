f1ebccd924db4a372853d028c91db161abd7b5ce
Lee-Ethan <ethanlee71400@gmail.com>
2019-11-03 12:46:59 -0500

    Changed expenses modal and html

--- FinancialDashboard/Expenses.js
+++ FinancialDashboard/Expenses.js
@@ -7,7 +7,6 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   var modal = document.getElementById("expensesModal");
-  var data = document.getElementById()
   modal.style.display = "block";
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
